347df8825e07b4b485eaa84be8b292918c05d41b
VencentEM <luxestore1997@gmail.com>
2024-10-30 21:25:09 +0000

    tasks

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,10 @@
+// Import jQuery
+import $ from 'jquery';
+
+// Use jQuery to add three paragraphs to the body
+$(document).ready(function() {
+    $('body').append('<p>Holberton Dashboard</p>');
+    $('body').append('<p>Dashboard data for the students</p>');
+    $('body').append('<p>Copyright - Holberton School</p>');
+});
+
