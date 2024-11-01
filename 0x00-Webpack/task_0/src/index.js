df1dbcaa4a11e76eccde2734627ce43d959d39be
VencentEM <luxestore1997@gmail.com>
2024-11-01 00:20:56 +0000

    Set up basic Webpack project with jQuery

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,10 +1,5 @@
-// Import jQuery
 import $ from 'jquery';
 
-// Use jQuery to add three paragraphs to the body
-$(document).ready(function() {
-    $('body').append('<p>Holberton Dashboard</p>');
-    $('body').append('<p>Dashboard data for the students</p>');
-    $('body').append('<p>Copyright - Holberton School</p>');
-});
-
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
