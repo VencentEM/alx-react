7b8600cf3cc009a46926b73cb67955169faa68f4
VENCENT <luxestore1997@gmail.com>
2024-11-16 22:45:54 +0100

    Add email and password inputs with OK button to App.js

---
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import logo from './logo.jpg'
+import { getFullYear, getFooterCopy } from './utils'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>School dashboard</h1>
+        </header>
+        <body className="App-body">
+          <p>Login to access the full dashboard</p>
+
+          {/* Add email input */}
+          <label htmlFor="email" onClick={() => {
+            // select corresponding input
+            document.getElementById('email').focus();
+          }}>Email</label>
+          <input type="email" id="email" />
+
+          {/* Add password input */}
+          <label htmlFor="password" onClick={() => {
+            // select corresponding input
+            document.getElementById('password').focus();
+          }}>Password</label>
+          <input type="password" id="password" />
+
+          {/* Add OK button */}
+          <button>OK</button>
+
+        </body>
+        <footer className="App-footer">
+          <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+        </footer>
+      </div>
+    )
+  }
+}
