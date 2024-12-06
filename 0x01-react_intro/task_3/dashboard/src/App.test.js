87f08feea4c6c365b35be93fdee2ed1f55b942e9
VENCENT <luxestore1997@gmail.com>
2024-12-06 21:46:03 +0100

    task

--- 0x01-react_intro/task_3/dashboard/src/App.test.js
+++ 0x01-react_intro/task_3/dashboard/src/App.test.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import { shallow } from 'enzyme';
 import App from './App';
+import { shallow } from 'enzyme';
 
-describe('<App />', () => {
+describe('App tests', () => {
     it('renders without crashing', () => {
-        const wrapper = shallow(<App />);
-        expect(wrapper.exists()).toBe(true);
+        const component = shallow(<App />);
+
+        expect(component).toBeDefined();
+    });
+    it('should render a div with the class App-header', () => {
+        const component = shallow(<App />);
+
+        expect(component.find('.App-header')).toBeDefined();
+    });
+    it('should render a div with the class App-body', () => {
+        const component = shallow(<App />);
+
+        expect(component.find('.App-body')).toBeDefined();
+    });
+    it('should render a div with the class App-footer', () => {
+        const component = shallow(<App />);
+
+        expect(component.find('.App-footer')).toBeDefined();
     });
 });
-/*this test just to check if enzyme work good */
